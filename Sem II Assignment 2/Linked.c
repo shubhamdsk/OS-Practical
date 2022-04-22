fe918182bbf894adf8ef514f8efb1d79f3d043aa
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-22 19:44:59 +0530

    Assignment on File

---
+++ Sem II Assignment 2/Linked.c
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+typedef struct dir
+{
+ char fname[20];
+ int start;
+ struct dir *next;
+}NODE;
+NODE *head,*last,*temp,*prev,*dell,*tmp;
+int n,bit[MAX],fb=0;
+
+void init()
+{
+ int i;
+ printf("Enter total no.of disk blocks:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+	bit[i]=rand()%2;
+ }
+}
+
+void show_bitvector()
+{
+ int i;
+ for(i=0;i<n;i++)
+  printf("%d ",bit[i]);
+ printf("\n");
+}
+
+void show_dir()
+{
+ NODE *temp;
+ int i;
+ printf("\n Directory "); 
+ printf("\n File Name     Allocated Block Number "); 
+ for(temp=head;temp!=NULL;temp=temp->next)
+  {
+  printf("\n   %s",temp->fname);  
+  printf("           %d",temp->start);
+  }
+ 
+
+printf("\n\n Allocated Blocks :  "); 
+for(temp=head;temp!=NULL;temp=temp->next)
+  {
+   printf("%d->",temp->start);
+  }
+ printf("NULL\n\n");
+}
+
+void create()
+{
+ NODE *p;
+ char fname[20];
+ int i,j,nob;
+ int fb=0;
+ printf("Enter file name:");
+ scanf("%s",fname);
+ printf("Enter no.of blocks:");
+ scanf("%d",&nob);
+ for(i=0;i<n;i++)
+ {
+  if(bit[i]==0) 
+    fb++; 
+ }
+if(nob>fb)
+ {
+  printf("Failed to create file %s\n",fname);
+  return;
+ }
+ else
+ {
+    for(i=0;i<n;i++)   //1 1 1 1 1 1 1 0 1 1 0 1 0 1 1 0 0 0 0 0 1 0 1 1 0 0 0 1 1 1 1 0 0 0 1 1 1 0 1 0 1 1 
+	{
+	if(bit[i]==0 && nob!=0) 
+         {
+		p = (NODE*)malloc(sizeof(NODE));   //i= 1 6             nob=4 3 2
+		strcpy(p->fname,fname);            //head= p(1)              last= p(1) p(6) 
+		nob--;                             //p->fname=F1
+		bit[i]=1;                           //p->start=1 6
+		p->start=i;                         //p->next=NULL
+		p->next=NULL;                       // 
+		if(head==NULL)
+                   head=p;                        
+		else
+        	  last->next=p;
+               last=p;
+	}
+       }
+    printf("File %s created successully.\n",fname);
+ } 
+}
+void delete()
+{
+   int i=0; 
+   NODE *p,*q;
+   char fname[20];
+	
+  temp=head;
+  printf("Enter file to be deleted:");
+  scanf("%s",fname);
+
+  while(temp!=NULL)
+    {
+	p = q = head;
+	while(p!=NULL)
+	{
+           if(strcmp(p->fname,fname)==0)
+			break;
+           q = p;
+           p = p->next;
+	}
+
+	if(p==NULL)
+	{
+	   return;
+	}
+
+
+	if(p==head)
+		head = head->next;
+	else if(p==last)
+	      {
+		last=q;
+		last->next=NULL;
+	      }
+	    else
+             {
+		q->next = p->next;
+             }
+        bit[p->start]=0;
+	free(p);
+   temp=temp->next;
+  }
+
+	printf("File %s deleted successfully.\n",fname);
+
+}
+
+  
+int main()
+{
+ int ch;
+ init();
+ while(1)
+ {
+  printf("1.Show bit vector\n");
+  printf("2.Create new file\n");
+  printf("3.Show directory\n");
+  printf("4.Delete file\n");
+  printf("5.Exit\n");
+  printf("Enter your choice (1-5):");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+  case 1:show_bitvector(); break;
+  case 2: create(); break;
+  case 3:show_dir(); break;
+  case 4: delete();break;
+  case 5: exit(0);
+  }
+}
+return 0;
+}
---
+++ Sem II Assignment 2/Sequence.c
@@ -0,0 +1,125 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int bv[50],i,st,j,len,c,k;
+char name[10][30];
+int start[10],length[10],num=0;
+
+
+
+void dislayBitVector()
+{
+  printf("\n Bit Vector contents \n");
+  for(i=0;i<50;i++)
+     printf(" %d ",bv[i]);
+}
+
+void creteFile()
+{
+    char temp[30];
+    printf("Enter the name of the file :");
+    scanf("%s",&temp);
+    for(int i=0;i<num;i++)
+            if(strcmp(name[i],temp)==0) 
+            {
+            printf("\n Filename already exists");
+            return;
+            }
+    strcpy(name[num],temp);
+    printf("Enter the start block of the file : ");
+    scanf("%d",&start[num]);
+    printf("Enter the length of the file : ");
+    scanf("%d",&length[num]);
+    
+    for(j=start[num];j<(start[num]+length[num]);j++)
+      if(bv[j]==1)
+        {  
+	printf("File can not be allocated...Block already allocated");
+        strcpy(name[j],"-");   
+        start[j]=0;
+        length[j]=0; 
+        return;
+        }
+    
+     for(j=start[num];j<(start[num]+length[num]);j++)
+       { 
+        bv[j]=1;
+        printf("\n%d->%d",j,bv[j]);
+        }
+      
+    num++;
+}
+
+void showDirectory()
+{
+  printf("Directory contents\n");
+    printf("%s%40s%40s\n","File Name","Start Block","Length");
+   
+    
+    for(int i=0;i<num;i++)
+    {
+        if(strcmp(name[i],"-")!=0)
+            {
+                printf("%s%40d%40d\n",name[i],start[i],length[i]);
+            }
+    }
+}
+
+void deleteFile()
+{  
+    char temp[10];
+   
+    printf("\nEnter the name of the file to delete  :");
+    scanf("%s",&temp);
+    for(int i=0;i<num;i++)
+    {
+     if(strcmp(name[i],temp)==0)
+       {
+          
+          for(j=start[i];j<(start[i]+length[i]);j++)
+            {
+            bv[j]=0;
+            printf("\n%d->%d",j,bv[j]);
+            }
+          printf("\n File successfully deleted..");
+          strcpy(name[i],"-");   
+          start[i]=0;
+          length[i]=0; 
+          return;
+       }    
+    }
+  
+       printf("\n File not found..");
+      
+}
+
+int main()
+{
+  int ch=0;
+  for(i=0;i<50;i++)
+     bv[i]=0;
+   do
+   {
+   printf("\n *** Menu ****");
+   printf("\n 1. Show Bit Vector");
+   printf("\n 2. Create New File");
+   printf("\n 3. Show Directory");
+   printf("\n 4. Delete File");
+   printf("\n 5. Exit");
+   printf("\n Enter your choice : ");
+   scanf("%d",&ch);
+   switch(ch)
+    {
+    case 1: dislayBitVector();
+            break;
+    case 2: creteFile();
+            break;
+    case 3: showDirectory();
+            break;
+    case 4: deleteFile();
+            break;
+    //case 5: exit(0);
+    }       
+   }while(ch!=5);
+      
+}
