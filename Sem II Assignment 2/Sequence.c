52ed799ba26fceaf23ccf33c67ead704ae548d48
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-22 22:55:18 +0530

    Assignment on File

--- Sem II Assignment 2/Sequence.c
+++ Sem II Assignment 2/Sequence.c
@@ -5,14 +5,12 @@ int bv[50],i,st,j,len,c,k;
 char name[10][30];
 int start[10],length[10],num=0;
 
-
-
 void dislayBitVector()
 {
   printf("\n Bit Vector contents \n");
   for(i=0;i<50;i++)
      printf(" %d ",bv[i]);
-}
+}//dislayBitVector
 
 void creteFile()
 {
@@ -24,7 +22,7 @@ void creteFile()
             {
             printf("\n Filename already exists");
             return;
-            }
+            }//if
     strcpy(name[num],temp);
     printf("Enter the start block of the file : ");
     scanf("%d",&start[num]);
@@ -34,19 +32,18 @@ void creteFile()
     for(j=start[num];j<(start[num]+length[num]);j++)
       if(bv[j]==1)
         {  
-	printf("File can not be allocated...Block already allocated");
-        strcpy(name[j],"-");   
-        start[j]=0;
-        length[j]=0; 
-        return;
-        }
+                printf("File can not be allocated...Block already allocated");
+                strcpy(name[j],"-");   
+                start[j]=0;
+                length[j]=0; 
+                return;
+        }//if
     
      for(j=start[num];j<(start[num]+length[num]);j++)
        { 
         bv[j]=1;
         printf("\n%d->%d",j,bv[j]);
-        }
-      
+        }//for
     num++;
 }//creteFile
 
@@ -55,14 +52,13 @@ void showDirectory()
   printf("Directory contents\n");
     printf("%s%40s%40s\n","File Name","Start Block","Length");
    
-    
     for(int i=0;i<num;i++)
     {
         if(strcmp(name[i],"-")!=0)
             {
                 printf("%s%40d%40d\n",name[i],start[i],length[i]);
-            }
-    }
+            }//if
+    }//for
 }//showDirectory
 
 void deleteFile()
@@ -74,24 +70,21 @@ void deleteFile()
     for(int i=0;i<num;i++)
     {
      if(strcmp(name[i],temp)==0)
-       {
-          
+       {  
           for(j=start[i];j<(start[i]+length[i]);j++)
             {
             bv[j]=0;
             printf("\n%d->%d",j,bv[j]);
-            }
+            }//for
           printf("\n File successfully deleted..");
           strcpy(name[i],"-");   
           start[i]=0;
           length[i]=0; 
           return;
-       }    
-    }
-  
+       }//if  
+    }//for
        printf("\n File not found..");
-      
-}
+}//deleteFile
 
 int main()
 {
@@ -100,26 +93,25 @@ int main()
      bv[i]=0;
    do
    {
-   printf("\n *** Menu ****");
-   printf("\n 1. Show Bit Vector");
-   printf("\n 2. Create New File");
-   printf("\n 3. Show Directory");
-   printf("\n 4. Delete File");
-   printf("\n 5. Exit");
-   printf("\n Enter your choice : ");
-   scanf("%d",&ch);
-   switch(ch)
-    {
-    case 1: dislayBitVector();
-            break;
-    case 2: creteFile();
-            break;
-    case 3: showDirectory();
-            break;
-    case 4: deleteFile();
-            break;
-    //case 5: exit(0);
-    }       
+        printf("\n *** Menu ****");
+        printf("\n 1. Show Bit Vector");
+        printf("\n 2. Create New File");
+        printf("\n 3. Show Directory");
+        printf("\n 4. Delete File");
+        printf("\n 5. Exit");
+        printf("\n Enter your choice : ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+        case 1: dislayBitVector();
+                break;
+        case 2: creteFile();
+                break;
+        case 3: showDirectory();
+                break;
+        case 4: deleteFile();
+                break;
+        } //switch      
    }while(ch!=5);
       
-}
+}//main
